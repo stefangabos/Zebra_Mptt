9f5f64967bdc466721acd05e6525b2980f4eb680
writ3it <42292507+writ3it@users.noreply.github.com>
2019-09-06 22:32:56 +0200

    separate env data

--- tests/EnvironmentTest.php
+++ tests/EnvironmentTest.php
@@ -11,6 +11,7 @@ namespace ZebraTests;
 
 use PHPUnit\Framework\TestCase;
 use ZebraTests\Utility\DatabaseStubHelpMethodsInterface;
+use ZebraTests\Utility\Environment;
 use ZebraTests\Utility\MysqliInMemoryDummy;
 
 class EnvironmentTest extends TestCase
@@ -33,32 +34,16 @@ class EnvironmentTest extends TestCase
     }
 
     public function testInstallFileExists(){
-        $this->assertFileExists( $this->getInstallFilePath(), "Install file doesn't exists");
+        $this->assertFileExists( Environment::getInstallFilePath(), "Install file doesn't exists");
     }
 
     public function testInstall(){
-        $content = $this->getInstallFileContent();
+        $content = Environment::getInstallFileContent();
         $this->connection->query($content);
         $this->assertEquals(true, $this->connection->tableExists('mptt'),"Installing script doesn't work");
     }
 
-    private function getInstallFilePath():string{
-        return __DIR__.'/../install/mptt.sql';
-    }
 
-    private function getInstallFileContent():string
-    {
-        $content = file_get_contents($this->getInstallFilePath());
-        $content = $this->removeMysqlPart($content);
-        return rtrim(trim($content),';');
-    }
 
-    private function removeMysqlPart($content)
-    {
-        $replacement = [
-            ' ENGINE=InnoDB DEFAULT CHARSET=utf8'=>'',
-            'auto_increment'=>''
-        ];
-        return str_replace(array_keys($replacement),array_values($replacement),$content);
-    }
+
 }
