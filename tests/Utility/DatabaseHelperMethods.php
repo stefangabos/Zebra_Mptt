f11cb3f6528052b62cef09fc686d59ce50bb2747
writ3it <42292507+writ3it@users.noreply.github.com>
2019-09-07 16:37:16 +0200

    helper methods for tests

---
+++ tests/Utility/DatabaseHelperMethods.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: writ3it
+ * Date: 07.09.19
+ * Time: 16:29
+ */
+
+namespace ZebraTests\Utility;
+
+
+interface DatabaseHelperMethods
+{
+    public function tableExists(string $tableName):bool;
+}
