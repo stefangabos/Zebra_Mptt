e2efb173204f4a22118dbad083760f946e1b89b6
writ3it <42292507+writ3it@users.noreply.github.com>
2019-09-07 18:30:39 +0200

    default operator produced unexpected substr, that is better

--- src/AbstractDatabaseDriver.php
+++ src/AbstractDatabaseDriver.php
@@ -22,10 +22,10 @@ abstract class AbstractDatabaseDriver implements DatabaseDriverInterface
         static $operators = ['>'=>-1,'<'=>-1,'>='=>-2,'<='=>-2,'LIKE'=>-4]; // 'operator'=>-1*strlen('operator')
         foreach($operators as $operator=>$length){
             if (substr($name,$length) == $operator){
-                return $operator;
+                return [$operator,$length];
             }
         }
-        return '='; //default operator
+        return ['=',0]; //default operator
     }
 
     /**
@@ -41,9 +41,9 @@ abstract class AbstractDatabaseDriver implements DatabaseDriverInterface
         $output = array();
         foreach($conditions as $name=>$value){
             $trimmedName = trim($name);
-            $operator = $this->detectOperator($trimmedName);
-            $columnName = substr($trimmedName,0,-1*strlen($operator));
-            $output[] = array($columnName,$operator,$value);
+            $operatorDesc = $this->detectOperator($trimmedName);
+            $columnName = $operatorDesc[1] == 0 ? $trimmedName : substr($trimmedName,0,$operatorDesc[1]);
+            $output[] = array($columnName,$operatorDesc[0],$value);
         }
         return $output;
     }
